587bbf33de35f60c8c639f8a2088853b3c55afd2
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2025-06-04 10:09:47 +0900

    feat: pwa

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Button() {
   return (
     <div className="flex-1 px-4 py-3 text-xl text-center bg-gray-300 rounded-md cursor-pointer text-gray-50 hover:bg-gray-400">
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Smart Locker',
+        short_name: 'SmartLocker',
+        description: 'Smart Locker Application',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'marker.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'marker.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
